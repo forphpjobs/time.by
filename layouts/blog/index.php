fc6062416c204c7a0df30b44ff5fc801ebe80c6f
Alexander Ivanov <vsehsvt@bk.ru>
2021-04-04 14:14:23 +0300

    Добавил контент на страницу блога. Финальная часть проекта.

--- layouts/blog/index.php
+++ layouts/blog/index.php
@@ -1,5 +1,18 @@
 <?php include_once "layouts/layout/header.php" ?>
+<?php
 
+//$q=new DateTime($content[0]['date']);
+//$q1=$q->format('d-M-Y');
+//echo $q1.'<br>';
+//$q->modify('-4 Day');
+//$q1=$q->format('d-M-Y');
+//echo $q1.'<br>';
+//$q->add(new DateInterval('P1Y3M3D'));
+//$q1=$q->format('d-M-Y');
+//echo $q1;
+
+
+?>
     <?php foreach ($content as $item):?>
 
             <?php
